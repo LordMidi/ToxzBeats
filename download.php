d3daebc17499149f2a99a648976bafe44bd4e270
LordMidi <lordmidi@gmail.com>
2026-03-06 13:50:05 +0100

    updae to new layout, download wav, soundcloud preview

--- download.php
+++ download.php
@@ -1,10 +1,42 @@
 <?php
+
+function sanitize($value) {
+    $value = $value ?? '';
+    $value = preg_replace('/[^a-zA-Z0-9\/\.\(\)\s\-\_;]/', '', $value);
+    $value = substr($value, 0, 255);
+    return $value;
+}
+
 if (isset($_GET['file'])) {
-    $fileName = $_GET['file'] . '.mp3';
-    $file = 'beats/' . $fileName;
-    if (file_exists($file) && is_readable($file))  {
-        header('Content-type: application/mp3');
-        header("Content-Disposition: attachment; filename=\"$fileName\"");
-        readfile($file);
+    $fileName = basename($_GET['file']);
+    $fileName = sanitize($fileName);
+    $file = 'download/' . $fileName;
+    if (!empty($fileName) && file_exists($file) && is_file($file))  {
+
+        // log
+        $browser = sanitize($_SERVER['HTTP_USER_AGENT']);
+        $language = $_SERVER['HTTP_ACCEPT_LANGUAGE'];
+        $languageParts = explode(',', $language);
+        $language = reset($languageParts);
+        $language = sanitize($language);
+        $filePath = 'download/download.log';
+        $existingContent = file_exists($filePath) ? file_get_contents($filePath) : '';
+        file_put_contents($filePath, date('Y-m-d H:i:s') . ' | ' . $fileName . ' | ' .  $language . ' | ' . $browser . PHP_EOL . $existingContent) ;
+
+        // return wav with header to ensure download instead of media playback
+        $fileParts = explode('.', $fileName);
+        if (strtolower(end($fileParts)) === 'wav') {
+            header('Content-type: application/wav');
+            header("Content-Disposition: attachment; filename=\"$fileName\"");
+            header('Content-Length: ' . filesize($file));
+            header('Pragma: no-cache');
+            readfile($file);
+        } else {
+            header("Location: https://www.toxz.de/download/$fileName");
+        }
+
+    } else {
+        http_response_code(404);
+        echo "File not found.";
     }
 }
--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php
-include('beats.inc.php');
 $languageCode = 'de';
-$title = 'Kostenlose Musik, Beats & Instrumentale | Toxz Beats';
-$metaDescription = 'Kostenlose Downloads von Gema freien Beats & Instrumentals für deine Videos oder Rap Tracks.';
+$title = 'Kostenlose Musik, Free Beats & Instrumentale | Toxz Beats';
+$metaDescription = 'Kostenlose Downloads von Gema freien Free Beats & Instrumentals für deine Videos oder Rap Tracks.';
 $language = '<a class="language" href="en">english version</a>';
 $intro = '
     <h2>Toxz - Beats made in Berlin!</h2>
@@ -18,7 +17,7 @@ $seo = '
   </p>
   <p>
      Egal, ob als Intro oder Hintergrundmusik für ein <strong>Graffiti Video</strong> oder <strong>Let\'s Play Stream</strong>, Toxz Beats bietet eine eigene actiongeladene Mischung aus dem Bereich <strong>Electro Breakbeat</strong> und Hip Hop.
-     Lade dir einfach die Musik, die dir gefällt im <strong>High Quality WAV</strong> und MP3 Format auf dein Device.
+     Lade dir einfach die Musik, die dir gefällt im <strong>High Quality WAV</strong> auf dein Device.
   </p>
   <p>
      <strong>Ohne Anmeldung oder Abo</strong>! Content Creator und Rapper laden ihren Sound auf TOXZ.DE - jetzt!
@@ -27,12 +26,8 @@ $seo = '
 $license = '
   <h2>Lizenz</h2>
   <p>
-    Die auf dieser Webseite zum <strong>Download</strong> angebotene Musik wird unter der <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/deed.de">Attribution-NonCommercial-ShareAlike</a>-Lizenz veröffentlicht.
-    Bitte beachte diese Lizenzbedingungen, falls du die Musik verwenden möchtest.
-  </p>
-  <p>
-    Ohne Zustimmung von meiner Seite, darf die Musik für <strong>kommerzielle Projekte</strong> <u>nicht</u> verwendet werden!
-    Kontaktiere mich dafür einfach auf <a href="https://www.instagram.com/toxzbeats/">Instagram</a>.
+    Die auf dieser Webseite zum <strong>Download</strong> angebotene Musik wird unter der <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/deed.de">Attribution-NonCommercial-ShareAlike-Lizenz</a> veröffentlicht.
+    Bitte beachte diese Lizenzbedingungen, falls du die Musik verwenden möchtest. Einfachste Variante ist eine Erwähnung - "Musik: toxz.de".
   </p>
 ';
 $download = '<p>Klicke den Download Button zum herunterladen<p>';
@@ -63,36 +58,17 @@ if (isset($_GET['en'])) {
     </p>
     <p>
       Whether as an intro or background music for a <strong>graffiti video</strong> or <strong>Let\'s Play stream</strong>, Toxz Beats offers its own action-packed mix of electro breakbeat and hip hop.
-      Simply download the music you like to your device in <strong>high quality WAV and MP3 format</strong>.
-    </p>
-    <p>
-    <strong>Without registration or subscription</strong>! Content creators and rappers download their sound on TOXZ.DE - now!
+      Simply download the music you like to your device in <strong>high quality WAV format</strong>.
     </p>
   ';
   $license = '
     <h2>License</h2>
     <p>
-      The music on this website is published under the <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/">Attribution-NonCommercial-ShareAlike</a> license.
-      Please be aware of the licence conditions if you want to use this music.
-    </p>
-    <p>
-      Without permission you are <u>not</u> allowed to use the music in <strong>commercial projects</strong>!
-      Please contact me via <a href="https://www.instagram.com/toxzbeats/">Instagram</a>.
+      The music on this website is published under the <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/">Attribution-NonCommercial-ShareAlike license</a>.
+      Please be aware of the licence conditions if you want to use this music. The simplest way to give credits is: "music: toxz.de/en".
     </p>
   ';
   $download = '<p>Click the download button for downloading<p>';
-}
-
-/**
- * Format the beat name of display purposes.
- * @param $name
- * @return string
- */
-function formatBeatName($name)
-{
-  $name = str_replace('Toxz - ', '', $name);
-  $name = preg_split('/(?=_)/', $name, -1, PREG_SPLIT_NO_EMPTY)[0];
-  return $name;
 }
 ?>
 <!doctype html>
@@ -104,8 +80,7 @@ function formatBeatName($name)
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="theme-color" content="#000000">
-  <link rel="stylesheet" type="text/css" href="css/theme.css">
-  <link rel="stylesheet" type="text/css" href="css/custom.css">
+  <link rel="stylesheet" type="text/css" href="css/styles.css">
   <meta property="og:title" content="Toxz Beats">
   <meta property="og:description" content="<?= $metaDescription; ?>">
   <meta property="og:image" content="http://www.toxz.de/images/ogimage.jpg">
@@ -113,161 +88,58 @@ function formatBeatName($name)
 
 <body>
   <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-  <div class="container text-right">
-    <?= $language; ?>
-    <div class="social">
-      <a class="soundcloud" href="https://soundcloud.com/toxz-beats">
-        <img src="images/soundcloud.svg" alt="Soundcloud">
-      </a>
-      <a class="spotify" href="https://open.spotify.com/artist/46hYC3wABVTZ75uKiyahQt">
-        <img src="images/spotify.svg" alt="Spotify">
-      </a>
-      <a class="instagram" href="https://www.instagram.com/toxzbeats/">
-        <img src="images/instagram.svg" alt="Instagram">
-      </a>
-    </div>
-  </div>
-  <div class="container">
-    <div class="jumbotron">
-      <?= $seo; ?>
-    </div>
-  </div>
-  <div class="container">
-    <img class="toxz" src="images/toxz.svg" alt="Toxz Beats">
-  </div>
-  <div class="container">
-    <div class="jumbotron">
-      <?= $intro; ?>
-    </div>
-  </div>
-  <div class="container">
-    <div class="jumbotron">
-      <?= $license; ?>
-    </div>
-  </div>
-  <div class="container album" id="hardkinderchor">
-    <div class="jumbotron">
-      <h2>Toxz - Hard Kinderchor (2025)</h2>
-      <div class="row">
-        <div class="col-md-6">
-          <img class="cover" src="images/Toxz%20-%20Hard%20Kinderchor.jpg" alt="Toxz - Hard Kinderchor">
-          <a class="btn btn-success mb-1" href="download/Toxz%20-%20Hard%20Kinderchor.zip">
-            <img src="images/download.svg" alt="download">
-            Download (311MB)
-          </a>
-        </div>
-        <div class="col-md-6">
-          <iframe height="575" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/2000288604&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=false"></iframe>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="container album" id="cryptobreaksvol1">
-    <div class="jumbotron">
-      <h2>Toxz - Crypto Breaks Vol. 1 (2024)</h2>
-      <div class="row">
-        <div class="col-md-6">
-          <img class="cover" src="images/Toxz%20-%20Crypto%20Breaks%20Vol.%201.jpg" alt="Toxz - Crypto Breaks Vol. 1">
-          <a class="btn btn-success mb-1" href="download/Toxz%20-%20Crypto%20Breaks%20Vol.%201.zip">
-            <img src="images/download.svg" alt="download">
-            Download (637MB)
-          </a>
-        </div>
-        <div class="col-md-6">
-          <iframe height="575" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1780763301&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=false"></iframe>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="container album" id="freshfromthetrash">
-    <div class="jumbotron">
-      <h2>Toxz - Fresh From The Trash (2023)</h2>
-      <div class="row">
-        <div class="col-md-6">
-          <img class="cover" src="images/Toxz%20-%20Fresh%20From%20The%20Trash.jpg" alt="Toxz - Fresh From The Trash">
-          <a class="btn btn-success mb-1" href="download/Toxz%20-%20Fresh%20From%20The%20Trash.zip">
-            <img src="images/download.svg" alt="download">
-            Download (342MB)
-          </a>
-        </div>
-        <div class="col-md-6">
-          <iframe height="575" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1546064134&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=false"></iframe>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="container">
-    <div class="jumbotron">
-      <h2>single beats - WAV</h2>
-      <div class="soundclouddownload">
-        <img src="images/soundclouddownload.png" alt="">
-        <?= $download; ?>
-      </div>
-      <iframe height="650" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1232039722&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=false"></iframe>
-    </div>
-  </div>
-  <div class="beats container">
-    <div class="jumbotron">
-      <h2>single beats - MP3</h2>
-      <div class="row">
-        <? foreach ($beats as $beat) : ?>
-          <div class="col-sm-6 col-md-4">
-            <div class="thumbnail">
-              <div class="play" data-background-image="beats/<?= rawurlencode($beat); ?>.jpg">
-                <a class="inline-playable btn btn-default" href="beats/<?= rawurlencode($beat); ?>.mp3" role="button" aria-label="play"></a>
-              </div>
-              <p class="caption clearfix">
-                <a class="download btn btn-success" href="download.php?file=<?= rawurlencode($beat); ?>" role="button" aria-label="download" target="_blank"></a>
-                <?= formatBeatName($beat); ?>
-              </p>
-            </div>
-          </div>
-        <?php endforeach; ?>
-      </div>
-    </div>
-  </div>
-  <div class="container">
-    <img class="piece" src="images/piece.svg" alt="Toxz Graffiti">
-  </div><script src="soundmanager2/min.js"></script>
-  <script>
-    // lazy loading of beat images
-    var handler = function() {
-      var elements = document.getElementsByClassName('play');
-      for (var property in elements) {
-        (function() {
-          if (!elements.hasOwnProperty(property)) return;
-          var element = elements[property];
-          var rect = element.getBoundingClientRect();
-          if (
-            rect.top >= -150 &&
-            rect.left >= -150 &&
-            rect.bottom <= (window.innerHeight + 150 || document.documentElement.clientHeight + 150) &&
-            rect.right <= (window.innerWidth + 150 || document.documentElement.clientWidth + 150)
-          ) {
-            var backgroundImageUrl = element.getAttribute('data-background-image');
-            var image = new Image();
-            image.onload = function() {
-              element.style.backgroundImage = 'url(' + backgroundImageUrl + ')';
-            };
-            image.src = backgroundImageUrl;
-          }
-        })();
-      }
-    };
-    if (window.addEventListener) {
-      addEventListener('DOMContentLoaded', handler, false);
-      addEventListener('load', handler, false);
-      addEventListener('scroll', handler, false);
-      addEventListener('resize', handler, false);
 
-      // IE9+ :(
-    } else if (window.attachEvent) {
-      attachEvent('onDOMContentLoaded', handler);
-      attachEvent('onload', handler);
-      attachEvent('onscroll', handler);
-      attachEvent('onresize', handler);
-    }
-  </script>
+  <div class="container social">
+    <?= $language; ?>
+    <a href="https://soundcloud.com/toxz-beats">
+      <img src="images/soundcloud.svg" alt="Soundcloud">
+    </a>
+    <a href="https://open.spotify.com/artist/46hYC3wABVTZ75uKiyahQt">
+      <img src="images/spotify.svg" alt="Spotify">
+    </a>
+    <a href="https://www.instagram.com/toxzbeats/">
+      <img src="images/instagram.svg" alt="Instagram">
+    </a>
+  </div>
+
+  <div class="container">
+    <?= $seo; ?>
+  </div>
+
+  <div class="container piece">
+    <img src="images/piece.svg" alt="Toxz Graffiti">
+  </div>
+
+  <div class="container">
+    <?= $intro; ?>
+  </div>
+
+  <div class="container">
+    <?= $license; ?>
+  </div>
+
+  <div class="container">
+    <h2>Downloads</h2>
+  </div>
+
+  <? foreach (json_decode(file_get_contents('downloads.json'), true) as $download) : ?>
+    <div class="container">
+      <? if ($download['type'] === 'track'): ?>
+        <iframe tabindex="-1" height="166" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/soundcloud%253Atracks%<?= $download['soundcloudId']; ?>&color=%23ff5500&auto_play=false&hide_related=true&show_comments=true&show_user=true&show_reposts=false&show_teaser=false"></iframe>
+      <? endif; ?>
+      <? if ($download['type'] === 'album'): ?>
+        <iframe tabindex="-1" height="400" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/soundcloud%253Aplaylists%<?= $download['soundcloudId']; ?>&color=%23ff5500&auto_play=false&hide_related=true&show_comments=true&show_user=true&show_reposts=false&show_teaser=false"></iframe>
+      <? endif; ?>
+      <a class="download" href="download.php?file=<?= rawurlencode($download['file']); ?>" role="button" aria-label="download <?= $download['title']; ?>" title="download <?= $download['title']; ?>" target="_blank" rel="nofollow">
+        <img src="images/download.svg" alt=""> <?= $download['title']; ?>
+      </a>
+    </div>
+  <?php endforeach; ?>
+
+  <div class="container logo">
+    <img src="images/toxz.svg" alt="Toxz Beats">
+  </div>
+
   <script>
     (function(i, s, o, g, r, a, m) {
       i['GoogleAnalyticsObject'] = r;
